90e83e2767885e5ed8bdf3d51a7579207ecadde4
John <JLSenar@gail.com>
2017-02-23 01:30:29 -0800

    vision api call works on file 'upload'

--- public/js/fileUpload.js
+++ public/js/fileUpload.js
@@ -1,3 +1,5 @@
+
+
 $("#file-select").change(function(e) {
  function initOnReaderLoaded(img, reader){
     return function(){
@@ -7,6 +9,7 @@ $("#file-select").change(function(e) {
  for (var i = 0; i < e.originalEvent.srcElement.files.length; i++) {
 
     var file = e.originalEvent.srcElement.files[i];
+
 
      var div = document.createElement("div");
      div.id = i;
@@ -28,3 +31,32 @@ $("#file-select").change(function(e) {
      //var container = document.getElementById("images-container");
      //container.appendChild(div);
 } });
+
+function categorize() {
+        var params = {
+            // Request parameters
+        };
+
+        var filething = document.getElementById('file-select').files[0];
+
+
+        $.ajax({
+            url: "https://westus.api.cognitive.microsoft.com/vision/v1.0/tag?" + $.param(params),
+            beforeSend: function(xhrObj){
+                // Request headers
+                xhrObj.setRequestHeader("Content-Type","application/octet-stream");
+                xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key","a4b21213a6ff49a991c1fdc5d0699d70");
+            },
+            type: "POST",
+            processData: false,
+            // Request body
+            data: filething//makeblob( URL.createObjectURL(filething))
+        })
+        .done(function(data) {
+            console.log("coolio");
+        })
+        .fail(function() {
+            alert("Error creating tags");
+        });
+    }
+
