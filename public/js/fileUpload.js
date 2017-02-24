1cd5dec1abca9c4444ef36d4688557f6e426cc0b
John <JLSenar@gail.com>
2017-02-23 20:58:30 -0800

    Face detection and identity connected to modal

--- public/js/fileUpload.js
+++ public/js/fileUpload.js
@@ -57,7 +57,7 @@ function categorize() {
             var htmlData ='';
             for (var i = 0; i < data["tags"].length; i++){
                 if (i == data["tags"].length-1){
-                    htmlData += data["tags"][i].name;
+                    htmlData += data["tags"][i].name+ ', ';
                 }
                 else{
                     htmlData += data["tags"][i].name + ', ';
@@ -66,7 +66,7 @@ function categorize() {
             htmlData += '</p>';
             //$('#uploadModal').modal('show'); 
             //alert(JSON.stringify(data["tags"]));
-            $("#getCode").html(htmlData);
+            $("#getCode").append(htmlData);
             $("#getCodeModal").modal('show');
         })
         .fail(function() {
@@ -93,12 +93,18 @@ function categorize() {
                 "maxNumOfCandidatesReturned":1,
                 "confidenceThreshold": 0.5
             };
+            if (data.length == 0){
+                console.log("No faces detected");
+                $("#getCode").append("<p>No faces could be detected</p>");
+                return;
+            }
             for (var i = 0; i < data.length; i++){
                 input['faceIds'].push(data[i].faceId);
             }
             var inputString = JSON.stringify(input);
 
              // .then() returns a new promise
+            if (data != null) {
              return $.ajax({
                  url: "https://westus.api.cognitive.microsoft.com/face/v1.0/identify?" + $.param(params),
             beforeSend: function(xhrObj){
@@ -109,12 +115,52 @@ function categorize() {
             type: "POST",
             // Request body
             data: inputString,
-        })
+        })}
         });
 
     a2.done(function(data) {
-        
-        console.log(data);
-    });
+        var person = [];
+        console.log("data");
+        for (var i = 0; i < data.length; i++){
+            if (data[i].candidates.length){
+                person.push(data[i].candidates[0].personId);
+            }
+        }
+        var htmlPerson = "<p>We found ";
+        if (person.length==0){
+            htmlPerson += "no known faces in this photo</p>";
+            $("#getCode").append("<p>All faces in photo are unknown</p>");
+            console.log("No Faces Identified");
+        }
+        else {
+            //$("#getCode").append("<p>We found ");
+            for (var i = 0; i < person.length; i++){
+                $.ajax({
+                    url: "https://westus.api.cognitive.microsoft.com/face/v1.0/persongroups/team5/persons/" + person[i] + "?" + $.param(params),
+                    beforeSend: function(xhrObj){
+                    // Request headers
+                        xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key","a901709024084f4593133346ffc04c2c");
+                    },
+                    type: "GET"//,
+                    // Request body
+                    //data: ,
+                })
+                .done(function(data) {
+                    //htmlPerson += data["name"]; 
+                    /*if (i == person.length-1){
+                        htmlPerson += " in this photo</p>";
+                    }*/
+                    $("#getCode").append(data["name"] + ", ");
+                    $("#getCodeModal").modal('show');
+                    console.log(htmlPerson);
+                })
+                .fail(function() {
+                    console.log("Identify Failed");
+                });
+                }
+            }
+            
+            //$("#getCodeModal").modal('show');
+        });
 }
 
